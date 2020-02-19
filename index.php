7d413d8b78c5c8b036f7fc63b09d91fbfa1505ed
Murilocrispim <crispimmurilo@hotmail.com>
2020-02-19 16:32:59 -0300

    Teste2.0

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
     <body>
         <h1>Aqui estarão os links dos meus exercicios</h1>
 
-        <a href="2020.02.19">19/02/2020</a>
+        <a href="2020.02.19/barbearia">19/02/2020</a>
     </body>
 </html>
